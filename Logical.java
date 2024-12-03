1a6b6ac0f290a4fb0ffe8133b313ece957d03d40
mraji324 <rajuhindur@gmail.com>
2024-12-03 11:17:57 +0400

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Logical
+{
+    public static void main(String[] args)
+    {
+        System.out.println(true&&true);
+        System.out.println(true&&false);
+        System.out.println(true||false);
+        System.out.println(!(true));
+    }
+}
