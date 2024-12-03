3780cb09bf1824ca9ebdb8a528517629abc2ef73
mraji324 <rajuhindur@gmail.com>
2024-12-03 11:16:30 +0400

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Assignment
+{
+   public static void main(String[] args)
+   {
+      int a=20;
+      int b=23;
+      System.out.println(a=b);
+      System.out.println(a+=b);
+      System.out.println(a-=b);
+      System.out.println(a*=b);
+      System.out.println(a/=b);
+      System.out.println(a%=b);
+      System.out.println(a++);
+      System.out.println(++a);
+      System.out.println(b--);
+      System.out.println(--b); 
+   }
+}
