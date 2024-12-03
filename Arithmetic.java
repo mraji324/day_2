58c47a4e3e6e405aca3f6920a839d48687142e85
mraji324 <rajuhindur@gmail.com>
2024-12-03 11:15:58 +0400

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        int a=23;
+        int b=34;
+        System.out.println("a+b="+a+b);
+        System.out.println("a*b="+a*b);
+        System.out.println("a/b="+a/b);
+        System.out.println("a%b="+a%b);
+    }
+}
