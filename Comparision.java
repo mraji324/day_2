59079e0315c07fcf910f8fc8a140a65f4a765963
mraji324 <rajuhindur@gmail.com>
2024-12-03 11:17:37 +0400

    Add files via upload

---
+++ Comparision.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Comparision
+{
+public static void main(String[] args)
+{
+    int a=45;
+    int b=65;
+    System.out.println(a=b);
+    System.out.println(a!=b);
+    System.out.println(a<=b);
+    System.out.println(a<b);
+    System.out.println(a>=b);
+    System.out.println(a>b);
+}
+}
